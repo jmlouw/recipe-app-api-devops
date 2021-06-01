028583614c6766c196a04569d4bb1ae168e4cfe9
jmlouw <jmlouw96@gmail.com>
2021-06-01 13:48:42 +0200

    added tags

--- deploy/main.tf
+++ deploy/main.tf
@@ -12,3 +12,13 @@ provider "aws" {
   region  = "us-east-1"
   version = "~> 2.50.0"
 }
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
+}
---
+++ deploy/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  default = "raad"
+}
+
+variable "project" {
+  default = "recipe-app-api-devops"
+}
+
+variable "contact" {
+  default = "jlouw@alphawave.co.za"
+}
